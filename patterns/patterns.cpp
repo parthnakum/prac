46ce10a79e622dd0ec6a88af008b4811052861b2
Parth Nakum <parthnakum021@gmail.com>
2026-04-05 04:13:20 +0530

    feat: add pattern5

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -32,7 +32,7 @@ void pattern3(int n)
         int print = 1;
         for (int j = 1; j <= i; j++)
         {
-            cout << print;
+            cout << print << " ";
             print++;
         }
         cout << endl;
@@ -45,7 +45,19 @@ void pattern4(int n)
     {
         for (int j = 1; j <= i; j++)
         {
-            cout << i;
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern5(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
         }
         cout << endl;
     }
@@ -94,6 +106,16 @@ int main()
     333
     4444
     55555
+    */
+
+    pattern5(n);
+    /*
+    Output:
+    * * * * *
+    * * * *
+    * * *
+    * *
+    *
     */
 
     return 0;
