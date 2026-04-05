740652b2adc6880ae458fe17229d9a49eda67664
Parth Nakum <parthnakum021@gmail.com>
2026-04-06 00:27:19 +0530

    feat: add pattern8 - 2

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -97,7 +97,7 @@ void pattern7(int n)
     }
 }
 
-void pattern8(int n)
+void pattern8Method1(int n)
 {
     for (int i = n; i >= 1; i--)
     {
@@ -110,6 +110,26 @@ void pattern8(int n)
             cout << "*";
         }
         for (int j = n + i - 1; j <= 2 * n - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern8Method2(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = i; j <= 2 * n - i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 2 * n - i; j <= 2 * n - 1; j++)
         {
             cout << " ";
         }
@@ -192,7 +212,8 @@ int main()
     *********
     */
 
-    pattern8(n);
+    pattern8Method1(n);
+    pattern8Method2(n);
     /*
     Output:
     *********
